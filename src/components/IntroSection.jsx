c8dcb15bc2277bb5cf21078631b9368b823a3563
Roberto Battaglia <battaroberto@gmail.com>
2020-04-05 10:24:32 +0100

    add styling to button and h2, h5

--- src/components/IntroSection.jsx
+++ src/components/IntroSection.jsx
@@ -7,19 +7,15 @@ import "primeflex/primeflex.css"
 class IntroSection extends React.Component {
   render () {
     return (
-      <div className="intro-section-container">
-        <div className="p-grid">
-          <div className="p-col">
-          <Typography variant="h2">
-            Roberto Battaglia
-          </Typography>
-          <Typography variant="h5" gutterBottom>
-            BACKEND DEVELOPER
-          </Typography>
+      <div className="intro-section-container vertical-center">
+        <div className="p-grid p-justify-center p-align-center">
+          <div className="p-col-12 p-md-8 p-lg-6 p-xl-5 horizontal-center">
+          <Typography variant="h2">Roberto Battaglia</Typography>
+          <Typography variant="h5" gutterBottom>BACKEND DEVELOPER</Typography>
           </div>
-        </div>
-        <div className="p-grid">
-          <Button content={<Typography variant="button">Open Resume</Typography>} color={'#1da1f2'}></Button>
+          <div className="p-col-12 p-md-8 p-lg-3 p-xl-2 horizontal-center">
+            <Button content={<Typography variant="button">Open Resume</Typography>} color={'#1da1f2'}/>
+          </div>
         </div>
       </div>
     )
