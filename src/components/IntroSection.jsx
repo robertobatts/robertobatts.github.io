f3469d3626c3da1b416803cc0ef2b2eb52e3d672
Roberto Battaglia <battaroberto@gmail.com>
2020-04-05 22:38:56 +0100

    improved intro section css

--- src/components/IntroSection.jsx
+++ src/components/IntroSection.jsx
@@ -9,11 +9,11 @@ class IntroSection extends React.Component {
     return (
       <div className="intro-section-container vertical-center">
         <div className="p-grid p-justify-center p-align-center">
-          <div className="p-col-12 p-md-8 p-lg-6 p-xl-5 horizontal-center">
+          <div className="p-col-12 p-md-10 p-lg-8 p-xl-7 horizontal-center">
           <Typography variant="h2">Roberto Battaglia</Typography>
           <Typography variant="h5" gutterBottom>BACKEND DEVELOPER</Typography>
           </div>
-          <div className="p-col-12 p-md-8 p-lg-3 p-xl-2 horizontal-center">
+          <div className="p-col-12 p-md-8 p-lg-3 p-xl-3 horizontal-center">
             <Button content={<Typography variant="button">Open Resume</Typography>} color={'#1da1f2'}/>
           </div>
         </div>
--- src/components/commons/Button.jsx
+++ src/components/commons/Button.jsx
@@ -4,7 +4,7 @@ import './Button.css';
 class Button extends React.Component {
   render() {
     return (
-      <div className="button flat-shadow-in" style={{'background-color': this.props.color}}>
+      <div className="button flat-shadow-in-out" style={{'background-color': this.props.color}}>
          {this.props.content}
       </div>
     );
