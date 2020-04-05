000facbfd78b0e110a89e0e4414fbfab6bc1e896
Roberto Battaglia <battaroberto@gmail.com>
2020-04-05 11:20:34 +0100

    init TopBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import IntroSection from './components/IntroSection'
+import TopBar from './components/TopBar'
 import './App.css';
 
 function App() {
   return (
     <div>
+      <TopBar/>
       <IntroSection/>
     </div>
   );
---
+++ src/components/TopBar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './TopBar.css'
+
+class TopBar extends React.Component {
+  render() {
+    return (
+      <div className="topbar-container vertical-center">
+        <div className="p-grid">
+          <div className="p-col-fixed">
+            <a href="#home">Home</a>
+          </div>
+          <div className="p-col-fixed">
+            <a href="#about">About me</a>
+          </div>
+          <div className="p-col-fixed">
+            <a href="#projects">Projects</a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default TopBar
