95b716a0e1b36b7b1eaf5c94fb91785ec8c47ad1
Roberto Battaglia <battaroberto@gmail.com>
2020-06-11 19:08:56 +0100

    Updates

---
+++ precache-manifest.4d53fec380f7ec4c00cb44a05ec6b04b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06790f152089de8ab6dd20fb162a2f52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba5cec46f3006a7d1c1c",
+    "url": "/static/css/2.04752bb3.chunk.css"
+  },
+  {
+    "revision": "6f959547c5e01e3210e2",
+    "url": "/static/css/main.d0d440e4.chunk.css"
+  },
+  {
+    "revision": "ba5cec46f3006a7d1c1c",
+    "url": "/static/js/2.19eda814.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.19eda814.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f959547c5e01e3210e2",
+    "url": "/static/js/main.ab0c1c0c.chunk.js"
+  },
+  {
+    "revision": "35d1daa3fc5f8c4257ce",
+    "url": "/static/js/runtime-main.72172a05.js"
+  }
+]);
