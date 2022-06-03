66d8a75eebd7044e588d23d0a3755f4d0490b219
Roberto Battaglia <battaroberto@gmail.com>
2022-06-03 14:24:50 +0100

    fix anchor links

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React from "react";
-import { HashRouter, Route, Routes } from "react-router-dom";
 import "./App.scss";
 import Main from "./containers/Main";
-import Resume from "./containers/resume/Resume";
 
 function App() {
   return (
     <div>
-      <HashRouter>
-        <Routes>
-        <Route exact path="/" element={<Main />} />
-        <Route path="/resume" element={<Resume />} />
-        </Routes>
-      </HashRouter>
+      <Main />
     </div>
   );
 }
